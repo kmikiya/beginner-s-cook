cf3261ce4427e26c2f241ba710596e2200e22090
kmikiya <k.mikiya97@gmail.com>
2021-12-05 14:57:52 +0000

    gomi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
+
+def set_search
+  #@search = Article.search(params[:q])
+  @search = Recipe.ransack(params[:q]) #ransackメソッド推奨
+  @search_recipes = @search.result.page(params[:page])
+end
 
   #ログイン後のリンク先設定
   def after_sign_in_path_for(resource)
