222117c349e18c196d5b57835d0964b2ea759139
kmikiya <k.mikiya97@gmail.com>
2021-12-13 10:58:31 +0000

    refile

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f591db81eabaab93e20f6de7b68df2af5fae630e907737dd3ea2e616a3c15449d063347ac827a4e48e7c4f5907f2acd7dc919bd8244d175a7bed1bcc261552f5'
