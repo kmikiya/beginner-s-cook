a164aa6a49e00e4039f3367f2a853f9ece0f14ee
kmikiya <k.mikiya97@gmail.com>
2021-12-19 04:19:52 +0000

    権限

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,8 @@ class Report < ApplicationRecord
   belongs_to :recipe
 
   attachment :image
+
+  validates :impression, presence: true
+  validates :evaluation, presence: true
+  #validates :image_id, presence: true
 end
