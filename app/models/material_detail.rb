e804f74febe66095cfcd68153ffb6b8be338d38a
kmikiya <k.mikiya97@gmail.com>
2021-12-25 09:58:45 +0000

    fixx

--- app/models/material_detail.rb
+++ app/models/material_detail.rb
@@ -2,6 +2,6 @@ class MaterialDetail < ApplicationRecord
   has_many :recipes, through: :materials, dependent: :destroy
   has_many :lists, dependent: :destroy
   has_many :recipes, dependent: :destroy
-  
+
   validates :name, presence: true
 end
