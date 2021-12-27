6d74812b0a3004d744047de6f0cc1f5290a2ba40
kmikiya <k.mikiya97@gmail.com>
2021-12-27 10:54:01 +0000

    mer

--- app/controllers/customer/recipes_controller.rb
+++ app/controllers/customer/recipes_controller.rb
@@ -22,7 +22,7 @@ class Customer::RecipesController < ApplicationController
     if check_validates && @recipe.save
       redirect_to root_path
     else
-      @error_message = "同じ材料は登録できません"
+      @error_message = "同じ材料は登録できません(デフォルトで表示しています)"
       # @explanation =  Recipe.new(recipe_params).explanations.build
       render 'new'
       #byebug
